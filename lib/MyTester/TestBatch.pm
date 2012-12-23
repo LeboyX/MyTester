fa7cb2563c67dff95e790ddd53210efb326c7131
Jason Kilroy <leboyX@gmail.com>
2012-12-23 01:36:13 -0800

    Remove exported function from ReportLine to make a dummy line. TestBatch does that itself now

--- lib/MyTester/TestBatch.pm
+++ lib/MyTester/TestBatch.pm
@@ -338,7 +338,7 @@ method generateReport (
    for my $test ($self->getTests()) {
       my $testReportLine = ($test->DOES("MyTester::Roles::CanGrade"))
          ? $test->genReport($test->testStatus())
-         : $self->generateDummyReportLine($indent, $columns, $delimiter);
+         : $self->_generateDummyReportLine($columns);
          
       $testReportLine->indent($testIndent);
       if (defined $delimiter) {
@@ -351,12 +351,9 @@ method generateReport (
    return $report;
 }
 
-method generateDummyReportLine (
-      PositiveInt $indent, 
-      PositiveInt $columns, 
-      RegexpRef $delimiter?) {
+method _generateDummyReportLine (
+      PositiveInt $columns) {
    my %args = (
-      indent => $indent,
       columns => $columns,
    );
    
@@ -365,10 +362,6 @@ method generateDummyReportLine (
       "or acted as a stepping stone to setup another test's environment.";
    
    my $reportLine = MyTester::Reports::ReportLine->new(%args);
-   
-   if (defined $delimiter) {
-      $reportLine->computeBrokenLineIndentation($delimiter);
-   }
    
    return $reportLine;
 }
